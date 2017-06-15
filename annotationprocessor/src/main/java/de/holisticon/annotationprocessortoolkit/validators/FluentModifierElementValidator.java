7a4711629b12b7e7e7b367c16c8608c0e36aa1bd
Tobias Stamann <tobias.stamann@holisticon.de>
2017-06-15 17:41:46 +0200

    Fixed issues with fluent api of FluentModifierElementValidator

--- annotationprocessor/src/main/java/de/holisticon/annotationprocessortoolkit/validators/FluentModifierElementValidator.java
+++ annotationprocessor/src/main/java/de/holisticon/annotationprocessortoolkit/validators/FluentModifierElementValidator.java
@@ -8,23 +8,24 @@ import javax.lang.model.element.Modifier;
 /**
  * Fluent and immutable validator for validation of {@link Modifier} of {@link Element}.
  */
-public class FluentModifierElementValidator<T extends Element> extends AbstractFluentElementValidator<FluentModifierElementValidator, T> {
+public class FluentModifierElementValidator extends AbstractFluentElementValidator<FluentModifierElementValidator, Element> {
 
 
-    public FluentModifierElementValidator(FrameworkToolWrapper frameworkToolWrapper, T elementToValidate) {
+    public FluentModifierElementValidator(FrameworkToolWrapper frameworkToolWrapper, Element elementToValidate) {
 
         super(frameworkToolWrapper, elementToValidate);
 
     }
 
-    private FluentModifierElementValidator(FluentModifierElementValidator<T> previousFluentModifierElementValidator, boolean currentResult) {
+    private FluentModifierElementValidator(FluentModifierElementValidator previousFluentModifierElementValidator, boolean currentResult) {
 
         super(previousFluentModifierElementValidator, currentResult);
 
     }
 
-    protected FluentModifierElementValidator<T> createNextFluentValidator(boolean nextResult) {
-        return new FluentModifierElementValidator<T>(this, nextResult);
+
+    protected FluentModifierElementValidator createNextFluentValidator(boolean nextResult) {
+        return new FluentModifierElementValidator(this, nextResult);
     }
 
 }
