fb409ca6950f97bbb695b8e5b6479391106659a2
Tobias Stamann <tobias.stamann@holisticon.de>
2017-12-04 00:58:15 +0100

    Changed default StandardLocation for created resources.

--- annotationprocessor/src/main/java/de/holisticon/annotationprocessortoolkit/generators/FileObjectUtils.java
+++ annotationprocessor/src/main/java/de/holisticon/annotationprocessortoolkit/generators/FileObjectUtils.java
@@ -16,6 +16,8 @@ public class FileObjectUtils {
 
     /**
      * Hidden constructor that prevents instantiation.
+     *
+     * @param frameworkToolWrapper the framework tool wrapper
      */
     private FileObjectUtils(FrameworkToolWrapper frameworkToolWrapper) {
         this.frameworkToolWrapper = frameworkToolWrapper;
@@ -24,11 +26,24 @@ public class FileObjectUtils {
     /**
      * Gets a SimpleResourceReader for reading a resource file.
      *
-     * @param fileName the file name of the resource to be opened
+     * @param fileName      the file name of the resource to be opened
+     * @param targetPackage the target package
      * @return SimpleResourceReader if resource exists and can be read (== not opened for writing)
      */
     public SimpleResourceReader getResource(String fileName, String targetPackage) throws IOException {
-        return new SimpleResourceReader(frameworkToolWrapper.getFiler().getResource(StandardLocation.SOURCE_OUTPUT, targetPackage != null ? targetPackage : "", fileName));
+        return getResource(fileName, targetPackage, StandardLocation.SOURCE_OUTPUT);
+    }
+
+    /**
+     * Gets a SimpleResourceReader for reading a resource file.
+     *
+     * @param fileName         the file name of the resource to be opened
+     * @param targetPackage    the target package
+     * @param standardLocation the location to read the resource from
+     * @return SimpleResourceReader if resource exists and can be read (== not opened for writing)
+     */
+    public SimpleResourceReader getResource(String fileName, String targetPackage, StandardLocation standardLocation) throws IOException {
+        return new SimpleResourceReader(frameworkToolWrapper.getFiler().getResource(standardLocation, targetPackage != null ? targetPackage : "", fileName));
     }
 
     /**
@@ -42,7 +57,7 @@ public class FileObjectUtils {
     }
 
     /**
-     * Creates a resource file resident in SOURCE_OUTPUT in root package with give name.
+     * Creates a resource file resident in CLASS_OUTPUT in root package with give name.
      *
      * @param fileName            the file name to use
      * @param targetPackage       the target package to use
@@ -52,11 +67,26 @@ public class FileObjectUtils {
      * @throws javax.annotation.processing.FilerException if the same pathname has already been created
      */
     public SimpleResourceWriter createResource(String fileName, String targetPackage, Element... originatingElements) throws IOException {
-        return new SimpleResourceWriter(frameworkToolWrapper.getFiler().createResource(StandardLocation.SOURCE_OUTPUT, targetPackage != null ? targetPackage : "", fileName, originatingElements));
+        return createResource(fileName, targetPackage, StandardLocation.CLASS_OUTPUT);
     }
 
     /**
-     * Creates a resurce file resident in SOURCE_OUTPUT in root package with give name.
+     * Creates a resource file resident in CLASS_OUTPUT in root package with give name.
+     *
+     * @param fileName            the file name to use
+     * @param targetPackage       the target package to use
+     * @param location            the location to write the file to.
+     * @param originatingElements the originating elements responsible for resource file creation.
+     * @return SimpleResourceWriter if it hasn't been created before.
+     * @throws IOException
+     * @throws javax.annotation.processing.FilerException if the same pathname has already been created
+     */
+    public SimpleResourceWriter createResource(String fileName, String targetPackage, StandardLocation location, Element... originatingElements) throws IOException {
+        return new SimpleResourceWriter(frameworkToolWrapper.getFiler().createResource(location, targetPackage != null ? targetPackage : "", fileName, originatingElements));
+    }
+
+    /**
+     * Creates a resource file resident in SOURCE_OUTPUT in root package with give name.
      *
      * @param fileName the file name to use
      * @return SimpleResourceWriter if it hasn't been created before.
